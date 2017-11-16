ca7cb499af0df625108244c30a858b8ba04d59b5
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 20:07:35 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@
 counter = 0
 
 def current_player(board)
-  if turn_count % 2 == 0
+  if turn_count(board) % 2 == 0
     "X"
-  elsif turn_count % 2 == 1
+  elsif turn_count(board) % 2 == 1
     "O"
   end
 end
