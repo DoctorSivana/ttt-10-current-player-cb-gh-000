7ee44c75d378b071097e1920328b23cbe181482a
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 20:23:22 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,16 @@
 def current_player(board)
-  if turn_count(board).to_i % 2 == 0
-    "X"
-  elsif turn_count(board).to_i % 2 == 1
-    "O"
-  end
+  count = turn_count(board)
+
+  (count % 2 ==0 )?
+      "X" : "O"
 end
 
 def turn_count(board)
   count = 0
-  board.each do |boards]
-    if boards == "X" || boards == "O"
-      count = count + 1
-
+  board.each do |item|
+    if item == "X" or item == "O"
+      count += 1
     end
-    return count
-
   end
-
+  return count
 end
