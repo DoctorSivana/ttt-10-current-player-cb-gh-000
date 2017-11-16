393a30af7fedbb0061f54888d6bf20d2a18ed51b
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 20:07:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,8 +11,8 @@ end
 
 def turn_count(board)
 
-  board.each do |board|
-      board == "X" || board == "O"
+  board.each do {|board|
+      if board == "X" || board == "O"
       counter += 1
-
+      } end
 end
