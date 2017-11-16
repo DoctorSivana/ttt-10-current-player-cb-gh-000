820be89c2302f478b78d20ccdbaeb96cc90f7ed8
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 20:16:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,3 @@
-
-counter = 0
-
 def current_player(board)
   if turn_count(board).to_i % 2 == 0
     "X"
@@ -9,10 +6,4 @@ def current_player(board)
   end
 end
 
-def turn_count(board)
 
-  board.each do |board|
-      if board == "X" || board == "O"
-        counter = 1 + counter
-      end
-end
